a80cddb4268247155ee80ce275d6216151bfc053
Ingo Schmied <ingo@ingoneering.com>
2021-10-24 10:38:26 -0400

    add icon to button

--- app.js
+++ app.js
@@ -1,2 +1,2 @@
 import { MDCRipple } from "@material/ripple/index";
-const ripple = new MDCRipple(document.querySelector(".foo-button"));
+const ripple = new MDCRipple(document.querySelector(".mdc-button"));
