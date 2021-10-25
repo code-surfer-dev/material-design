c99732f7ff225b1746b18956f54a1f996686240f
Ingo Schmied <ingo@ingoneering.com>
2021-10-25 05:45:33 -0400

    begin on top app bar

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
+import { MDCTopAppBar } from "@material/top-app-bar";
 import { MDCRipple } from "@material/ripple/index";
+
+const topAppBarElement = document.querySelector(".mdc-top-app-bar");
+const topAppBar = new MDCTopAppBar(topAppBarElement);
 const ripple = new MDCRipple(document.querySelector(".mdc-button"));
